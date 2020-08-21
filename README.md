4b2464d81427d8a63b22d0998cf5e24249be9d26
sadnessOjisan <sadness.ojisan@gmail.com>
2020-08-22 03:35:27 +0900

    it works

---
+++ README.md
@@ -0,0 +1,12 @@
+## mui select test
+
+mui の Select コンポーネントの挙動で実験
+
+```sh
+# buill
+
+npm run b
+
+# dev start
+npm run s
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
-console.log('hi')
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const el = () => {
+    return <div>hello world!</div>
+}
+
+ReactDOM.render(el(), document.getElementById('root'))
